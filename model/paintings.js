29f46cb9fb131b14b87c7e89030fa29e052a505f
Julie Napoli <njulie818@gmail.com>
2024-06-05 17:19:32 +0200

    feat(filters): fonctionnalité du filters qui marche. BACK-1

--- model/paintings.js
+++ model/paintings.js
@@ -1,8 +1,10 @@
 const { con } = require('../utils/db.js');
 
+let sqlToGetPaintingsWithImages = "SELECT p.*, (SELECT i.image FROM image i WHERE i.id_painting = p.id_painting ORDER BY i.id_painting LIMIT 1) AS first_image FROM painting p"
+
 const getAllPaintings = () => {
     return new Promise((resolve, reject) => {
-        con.query('SELECT p.*, (SELECT i.image FROM image i WHERE i.id_painting = p.id_painting ORDER BY i.id_painting LIMIT 1) AS first_image FROM painting p;', (err, rows, fields) => {
+        con.query(sqlToGetPaintingsWithImages + ';', (err, rows, fields) => {
             if (err) {
                 reject(err);
             } else {
@@ -29,38 +31,69 @@ const getPaintingFiltered = async (prices, color, sizes) => {
     let colorClause = '';
     let sizesClause = '';
 
+    let initialQuery = sqlToGetPaintingsWithImages + ' WHERE';
+    let query = sqlToGetPaintingsWithImages + ' WHERE';
+
     if (prices !== undefined && prices.length > 0) {
+        if (query !== initialQuery) {
+            query += 'AND';
+        };
         prices.forEach(element => {
-            pricesClause += " price = " + element + " OR "
+            pricesClause += ` price = '${element}' OR`;
         });
+
+        if (pricesClause.endsWith("OR")) {
+            pricesClause = pricesClause.slice(0, -2);
+        }
+
+        query += pricesClause;
     }
 
     if (color !== undefined && color.length > 0) {
+        if (query !== initialQuery) {
+            query += 'AND';
+        };
         color.forEach(element => {
-            colorClause += " color = " + element + " OR "
+            colorClause += ` color = '${element}' OR`;
         });
+
+        if (colorClause.endsWith("OR")) {
+            colorClause = colorClause.slice(0, -2);
+        }
+
+        query += colorClause;
     }
 
     if (sizes !== undefined && sizes.length > 0) {
+        if (query !== initialQuery) {
+            query += 'AND';
+        };
         sizes.forEach(element => {
-            sizesClause += " size = " + element + " OR "
+            sizesClause += ` size = '${element}' OR`;
         });
+
+        if (sizesClause.endsWith("OR")) {
+            sizesClause = sizesClause.slice(0, -2);
+        }
+
+        query += sizesClause;
+    }
+    query += ';'
+
+
+    if(pricesClause == '' && colorClause == '' && sizesClause == '') {
+        query = query.slice(0, -6);
     }
 
-    if (pricesClause.endsWith("OR ")) {
-        pricesClause = pricesClause.slice(0, -3);
-        console.log(pricesClause)
-    }
-
-    if (colorClause.endsWith("OR ")) {
-        colorClause = colorClause.slice(0, -3);
-        console.log(colorClause)
-    }
-
-    if (sizesClause.endsWith("OR ")) {
-        sizesClause = sizesClause.slice(0, -3);
-        console.log(sizesClause)
-    }
+    return new Promise((resolve, reject) => {
+        con.query(query, (err, rows, fields) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
   };
 
 module.exports = { getAllPaintings, getPaintingById, getPaintingFiltered };
